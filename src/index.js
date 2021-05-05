b356e37c414fefe9edd8e79625cdf53c911e4e15
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-05-05 17:40:42 -0500

    Dark mode toggle persists

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+import useToggle from './components/hooks/useToggle';
+
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
