f707294a5869c20f4d9b875f28b051f4284de392
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-05-05 17:52:59 -0500

    inline notes

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,17 +1,20 @@
 import {useState} from 'react';
 
 const useLocalStorage = (key, initialValue) => {
+   // Get from local storage by key
     const [value, setValue] = useState(()=>{
       if (localStorage.getItem(key)) {
         return JSON.parse(localStorage.getItem(key));
+        // Parse and return stored json
       }
-  
+  // if stored json undefined, return initialValue
       localStorage.setItem(key, JSON.stringify(initialValue));
       return(initialValue);
     });
   
     const setStoredValue = value => {
       setValue(value);
+      // Saves to local storage
       localStorage.setItem(key, JSON.stringify(value));
     }
   
--- src/components/hooks/useToggle.js
+++ src/components/hooks/useToggle.js
@@ -1,6 +1,7 @@
 import useLocalStorage from './useLocalStorage';
 
 const useToggle = (key, value) => {
+    // useLocalStorage hook persists toggle state through a page refresh.
     const [darkMode, setDarkMode] = useLocalStorage(key, value);
 
     return [darkMode, setDarkMode];
